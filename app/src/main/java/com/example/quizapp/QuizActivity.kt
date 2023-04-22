57ac51a17814b5d85a12b325a62fc54430914e12
ferhatseker180 <ferhatseker180@gmail.com>
2023-04-22 22:17:38 +0300

    Prepare to main design. Create the function but it wasn't arrange.

--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -3,26 +3,44 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_quiz.*
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition : Int = 1
+    private var mQuestionsList : ArrayList<Questions>? = null
+    private var mSelectedOptionPosition : Int = 0
+
+    private var buttonSubmit : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
-        val questionsList = Constants.getQuestions()
-     //   Log.e("Questions",questionsList.toString())
+        mQuestionsList = Constants.getQuestions()
+        //   Log.e("Questions",questionsList.toString())
+        setQuestion()
 
-        for (s in questionsList) {
-            Log.e("Question",s.questions)
-        }
 
-        var currentPosition = 0
-        val question : Questions = questionsList[currentPosition]
+    }
 
-        progressBar.progress = currentPosition
-        textViewProgress.text = "$currentPosition / ${progressBar.max}"
+    private fun setQuestion() {
 
+        val question: Questions = mQuestionsList!![mCurrentPosition - 1]
+
+        progressBar.progress = mCurrentPosition
+        textViewProgress.text = "$mCurrentPosition / ${progressBar.max}"
+        textViewQuestions.text = question.questions
+        textViewOptionOne.text = question.optionOne
+        textViewOptionTwo.text = question.optionTwo
+        textViewOptionThree.text = question.optionThree
+        textViewOptionFour.text = question.optionFour
+        imageView.setImageResource(question.image)
+        buttonSubmit = findViewById(R.id.buttonSubmit)
+    }
+
+    override fun onClick(v: View?) {
 
     }
 }
